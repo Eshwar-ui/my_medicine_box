2c8f93266bdb6fe10e25387543a4165ab601eabe
eshwar <srirameshwarchandra@gmail.com>
2025-03-22 10:33:09 +0530

    0.1

--- lib/presentation/components/Fab.dart
+++ lib/presentation/components/Fab.dart
@@ -1,6 +1,8 @@
-// ignore: file_names
+// ignore_for_file: file_names
+
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:my_medicine_box/presentation/pages/detail_page.dart';
 
@@ -36,7 +38,7 @@ class _FabState extends State<Fab> {
     }
   }
 
-  void _showImageSourceOptions() {
+  void _showImageSourceOptionsOld() {
     showModalBottomSheet(
       context: context,
       shape: const RoundedRectangleBorder(
@@ -70,22 +72,86 @@ class _FabState extends State<Fab> {
     );
   }
 
+  // Future<void> _pickImage(ImageSource source) async {
+  //   XFile? xfile = await imagePicker.pickImage(source: source);
+
+  //   if (xfile != null) {
+  //     File image = File(xfile.path);
+  //     Navigator.push(context, MaterialPageRoute(builder: (ctx) {
+  //       return DetailPage(image);
+  //     }));
+  //   } else {
+  //     // Show a message if the user cancels the image selection
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       const SnackBar(content: Text('No image selected')),
+  //     );
+  //   }
+  // }
+
+  void _showImageSourceOptions() {
+    showModalBottomSheet(
+      backgroundColor: Theme.of(context).colorScheme.secondary,
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (ctx) {
+        return SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: const Icon(Icons.camera_alt),
+                title: Text(
+                  'Camera',
+                  style: TextStyle(fontSize: 20.sp),
+                ),
+                onTap: () async {
+                  Navigator.pop(ctx); // Close the modal
+                  await _pickImage(ImageSource.camera);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo),
+                title: Text(
+                  'Gallery',
+                  style: TextStyle(fontSize: 20.sp),
+                ),
+                onTap: () async {
+                  Navigator.pop(ctx); // Close the modal
+                  await _pickImage(ImageSource.gallery);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-      autofocus: true,
-      backgroundColor: Theme.of(context).colorScheme.primary,
-      shape: const CircleBorder(
-        side: BorderSide(
-            color: Color(0xff1D3557),
-            width: 9,
-            strokeAlign: BorderSide.strokeAlignOutside),
-      ),
-      onPressed: _showImageSourceOptions,
-      child: Icon(
-        Icons.camera_alt,
-        color: Theme.of(context).colorScheme.inversePrimary,
-        size: 29,
+    final isKeyboardVisible = MediaQuery.of(context).viewInsets.bottom != 0;
+    return Visibility(
+      visible: !isKeyboardVisible,
+      child: FloatingActionButton(
+        isExtended: true,
+        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        shape: CircleBorder(
+          side: BorderSide(
+              color: const Color(0xff1D3557),
+              width: 8.w,
+              strokeAlign: BorderSide.strokeAlignOutside),
+        ),
+        onPressed: () async {
+          return _showImageSourceOptions();
+        },
+        child: Icon(
+          Icons.camera_alt,
+          color: Theme.of(context).colorScheme.inversePrimary,
+          size: 29,
+        ),
       ),
     );
   }
