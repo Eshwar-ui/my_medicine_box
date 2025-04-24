58f0063f3602022e80cd9932c995cd29b15a59b7
BhavaniSaranga12 <sarangabhavani1231@gmail.com>
2025-04-24 23:14:24 +0530

    upd

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\eshwar\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\eshwar\Desktop\PROJECTS\my_medicine_box"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Bhavani\OneDrive\Desktop\New folder (2)\my_medicine_box"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
